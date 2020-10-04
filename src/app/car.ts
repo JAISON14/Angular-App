ea81d0cec11ad87357049fb7bce2f2ea7bf2777b
Jaison Jose <jaison1414@gmail.com>
2020-10-04 12:36:21 +0530

    Car details,Filters and Car listcomponents implimented

---
+++ src/app/car.ts
@@ -0,0 +1,20 @@
+export class Car {
+      productname: string
+      img_url:string
+      quantity:string
+      Engine:string
+      EngineType:string
+      Fuel_Type:string
+      Max_Power:string
+      Max_Torque:string
+      Mileage:string
+      Driving_Range:string
+      Drivetrain:string
+      Transmission:string
+      Seating_Capacity:string
+      Price:boolean
+      Brand:boolean
+      Headlights:boolean
+      Warranty:boolean
+
+}
---
+++ src/app/filters.ts
@@ -0,0 +1,19 @@
+export class Filters {
+    displayproductname:boolean;
+    displayquantity:boolean;
+    displayEngine:boolean;
+    displayEngineType:boolean;
+    displayFuel_Type:boolean;
+    displayMax_Power:boolean;
+    displayMax_Torque:boolean;
+    displayMileage:boolean;
+    displayDriving_Range:boolean;
+    displayDrivetrain:boolean;
+    displayTransmission:boolean;
+    displaySeating_Capacity:boolean;
+    displayPrice:boolean;
+    displayBrand:boolean;
+    displayHeadlights:boolean;
+    displayWarranty:boolean;
+   
+}
