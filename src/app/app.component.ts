0aba4b4e916b2c04e490bc01e8d7ea50c7209570
Jaison Jose <jaison1414@gmail.com>
2020-10-21 00:32:40 +0530

    Profile/Login modified in nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MyserviceService } from './myservice.service';
 import { Product } from './Product';
 
 @Component({
@@ -6,16 +8,38 @@ import { Product } from './Product';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'assign4';
   company='Amazon'
   pname='jaison'
   flag=false
+  loginStatus:boolean=false
+  CurrentUser_name:string;
   latestproduct:Product
   onNotify(product:Product){
     this.latestproduct= product
     this.flag=true
     console.count("flag is "+this.flag)
   }
-  
+
+  loginCheck(){
+    this.loginStatus=this.productService.loginStatus()
+    console.log(this.loginStatus)
+    if(this.loginStatus)
+    {
+      this.router.navigate(['profile'])
+    }
+    if(!this.loginStatus)
+    {
+    this.router.navigate(['login'])
+    }
+  }
+  constructor(private route:ActivatedRoute,private router:Router,private productService:MyserviceService) { }
+  ngOnInit(): void {
+
+    this.loginStatus=this.productService.loginStatus()
+    this.CurrentUser_name=this.productService.getCurrentUser()
+
+  }
 }
+
